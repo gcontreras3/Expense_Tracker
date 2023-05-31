888ee0255c88d6f5b55aabe3602877e975169bbc
Ivan Contreras <gcontreras31996@gmail.com>
2023-05-30 21:16:26 -0500

    final edits

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,7 +1,38 @@
 const account = {
-    name: 'Ivan Contreras',
-    expenses: []
+name: 'Ivan Contreras',
+expenses: [],
+income: [],
+addExpense: function(description, amount){
+    this.expenses.push({
+        description: description,
+        amount: amount
+    })
+},
+addIncome: function(description, amount){
+    this.income.push({
+        description: description,
+        amount: amount
+    })
+},
+
+
+getAccountSummary: function(){
+    let totalExpenses = 0
+    let totalIncome = 0
+    let accountBalance
+    this.expenses.forEach(function(expense){
+        totalExpenses = totalExpenses + expense.amount
+    })
+    this.income.forEach(function(income){
+        totalIncome = totalIncome + income.amount
+    })
+    accountBalance = totalIncome - totalExpenses
+    return `${this.name} has ${totalExpenses} in expenses.
+    ${this.name} has a balance of ${accountBalance}. ${totalIncome} in income. ${totalExpenses} in expenses.`
+},
+
 }
+
 
 // Expense -> description, amount
 // addExpense -> description, amount
@@ -9,4 +40,13 @@ const account = {
 
 account.addExpense('Rent', 950)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
+
+// console.log(account.addExpense('Rent', 950))
+// 1. Add imcome array to account
+// 2. addIncome method -> description, amount
+// 3. Tweak getAccountSummary
+
+// Ivan Contreras has a balance of $10. $100 in income. $90 in expenses.
+account.addIncome('Job', 1000)
